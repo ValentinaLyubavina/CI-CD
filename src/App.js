88b6ad7ff3a51679d0f12d7715ed6c5a577165b8
ValentinaLyubavina <lyubavina_valentina@mail.ru>
2025-01-04 17:13:27 +0300

    fix test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Header } from "./layout/Header";
 import { Footer } from "./layout/Footer";
 import { Main } from "./layout/Main";
+import React from "react";
 
 function App() {
   return (
